213eeb2e9cf47a595fb7357c28dbe05036c4ba80
bcamachouscamayta <evabri2008cue@gamil.com>
2025-12-10 10:48:37 -0300

    signup anda

---
+++ lucienne/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,64 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+
+import bcrypt from "bcryptjs";
+
+
+const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        // Buscar usuario por email
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) return null;
+
+        // Comparar contraseña
+        const validPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!validPassword) return null;
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.user.id = token.id;
+      session.user.role = token.role;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- lucienne/app/api/auth/signup/route.js
+++ lucienne/app/api/auth/signup/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
-import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+
 
 export async function POST(req) {
   try {
